a59eef7d420f3dae8c40a9b9f3403d9665721a9a
emonmahdi <developeremon1997@gmail.com>
2025-07-08 22:21:44 +0600

    frontend project initialize with tailwind setup

---
+++ frontend/courier-parcel-management-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+ 
+import './App.css'
+
+function App() { 
+
+  return (
+     <>
+     <h2 className='bg-purple-500 py-3'>Courier Parcel Management App</h2>
+     </>
+  )
+}
+
+export default App
