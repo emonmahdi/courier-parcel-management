64a6d624d30c02e82104c9bf8c2b05d48b8412a4
emonmahdi <developeremon1997@gmail.com>
2025-07-09 23:19:33 +0600

    authSlice create done

---
+++ frontend/courier-parcel-management-frontend/src/redux/features/auth/authSlice.js
@@ -0,0 +1,62 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { authService } from "./authService";
+
+const initialState = {
+  user: null,
+  loading: false,
+  error: null,
+};
+
+export const login = createAsyncThunk("/auth/login", async (data, thunkAPI) => {
+  try {
+    return await authService.login(data);
+  } catch (err) {
+    return thunkAPI.rejectWithValue(err.response.data.message);
+  }
+});
+
+export const register = createAsyncThunk(
+  "/auth/register",
+  async (data, thunkAPI) => {
+    try {
+      return await authService.register(data);
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response.data.message);
+    }
+  }
+);
+
+export const logout = createAsyncThunk("/auth/login", async () => {
+  authService.logout();
+});
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // login
+      .addCase(login.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload.user;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+
+      // register
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null;
+      });
+  },
+});
+
+export default authSlice.reducer;
