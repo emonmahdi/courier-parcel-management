cca256110f00b6d3617447a1ca44045bbaa9348a
emonmahdi <developeremon1997@gmail.com>
2025-07-09 23:29:55 +0600

    useAuth create done

--- frontend/courier-parcel-management-frontend/src/hooks/useAuth.js
+++ frontend/courier-parcel-management-frontend/src/hooks/useAuth.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import { useSelector, useDispatch } from "react-redux";
+import { login, register, logout } from "../redux/features/auth/authSlice";
 
-const useAuth = () => {
-  return (
-    <div>useAuth</div>
-  )
-}
+export const useAuth = () => {
+  const { user, loading, error } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
-export default useAuth
+  return {
+    user,
+    loading,
+    error,
+    login: (data) => dispatch(login(data)),
+    register: (data) => dispatch(register(data)),
+    logout: () => dispatch(logout()),
+  };
+};
--- frontend/courier-parcel-management-frontend/src/redux/features/auth/authSlice.js
+++ frontend/courier-parcel-management-frontend/src/redux/features/auth/authSlice.js
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { authService } from "./authService";
 
+
 const initialState = {
   user: null,
   loading: false,
